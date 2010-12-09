f0aa8113ca2330822140225c1e62cae0f3b78eab
cadorn <christoph@christophdorn.com>
2010-12-09 14:35:54 -0800

    Various changes and additions

--- packages/example-basic/packages/page-top/lib/main.js
+++ packages/example-basic/packages/page-top/lib/main.js
@@ -1,8 +1,7 @@
 
 var PLUGIN = require('plugin/plugin');
-var INSIGHT = require('plugin/insight');
 var CONSOLE = require('plugin/console');
-var EVENTS = require('plugin/events');
+var BROWSER = require('plugin/browser');
 var JQUERY = require('jquery/jquery').jQuery;
 
 exports.main = function() {
@@ -14,22 +13,43 @@ exports.main = function() {
     // "content" is the root HTML element (body) to put markup into
     JQUERY("#content").html(
         [
-            '<div class="links"><a href="#" id="ping-server-link">Ping Server</a></div>',
-            '<div class="links"><a href="#" id="toggle-height-link">Toggle Height</a></div>',
+            '<div class="links">',
+            '<a href="#" id="ping-server-link">Ping Server</a>',
+            '<a href="#" id="toggle-height-link">Toggle Height</a>',
+            '<a href="#" id="new-tab-link">Open New Tab</a>',
+            '<a href="#" id="new-window-link">Open New Window</a>',
+            '<a href="#" id="set-url-link">Set Page URL</a>',
+            '</div>',
+            '<div class="icon"><img src="' + PLUGIN.getImageUrl("firephp.png") + '"/></div>',
             '<div class="notes">Open <i>Firebug Console</i> to see messages.</div>'
         ].join("\n")
     );
 
-    document.getElementById("ping-server-link").addEventListener("click", function() {
+    JQUERY("#ping-server-link").click(function() {
         CONSOLE.log(["send", {
             "said": "Hello World from Basic Example: " + new Date()
         }]);
-        INSIGHT.to("plugin").plugin().sendSimpleMessage({
+        PLUGIN.sendSimpleMessage({
             "said": "Hello World from Basic Example: " + new Date()
+        });
+        CONSOLE.log(["send", {
+            "respond": true
+        }]);
+        PLUGIN.sendSimpleMessage({
+            "respond": true
+        }, function(response) {
+            CONSOLE.log(["response", response.data]);
         });
     }, false);
 
-    document.getElementById("toggle-height-link").addEventListener("click", function() {
+    // listen for messages from server
+    PLUGIN.addListener("message", function(message) {
+        if(message.type=="simple") {
+            CONSOLE.log(["received", message.data]);
+        }
+    });
+
+    JQUERY("#toggle-height-link").click(function() {
         PLUGIN.getHeight(function(height) {
             if(height==50) {
                 PLUGIN.setHeight(100);
@@ -39,9 +59,16 @@ exports.main = function() {
         });
     }, false);
 
-    // listen for messages from server
-    EVENTS.addListener("message", function(message) {
-        CONSOLE.log(["received", message.data]);
-    });
+    JQUERY("#new-tab-link").click(function() {
+        BROWSER.newTab("http://www.firephp.org/");
+    }, false);
+
+    JQUERY("#new-window-link").click(function() {
+        BROWSER.newWindow("http://www.firephp.org/");
+    }, false);
+
+    JQUERY("#set-url-link").click(function() {
+        BROWSER.setUrl("http://www.firephp.org/");
+    }, false);
 
 }
--- packages/example-basic/tests/php/FirePHPTest.inc.php
+++ packages/example-basic/tests/php/FirePHPTest.inc.php
@@ -2,7 +2,6 @@
 
 $plugin = FirePHP::to("plugin")->plugin('BasicExample');
 $plugin->register(array(
-    'container' => 'page-top',
     'class' => 'FirePHP_UIPlugins_ExampleBasic_Plugin',
     'file' => dirname(dirname(dirname(__FILE__))) . '/lib/Plugin.php',
     'forceReload' => true
